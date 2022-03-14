0bebf674504ba43f5a9887a96c05ca919da1c99d
Igor de Paula <principe.borodin@gmail.com>
2022-03-14 11:17:55 -0300

    try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './users'
+import { PostList, PostEdit, PostCreate } from './posts';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from "./Dashboard";
+import authProvider from "./authProvider";
+import buildGraphQLProvider from 'ra-data-graphql';
+import {createNetworkInterface} from 'react-apollo';
+//https://api.github.com/graphqlg
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/*const dataProvider = buildGraphQLProvider({
+    client: {
+        networkInterface: createNetworkInterface({
+            uri: 'https://api.github.com/graphql',
+        }),
+    },
+});*/
+//const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => {
+    const [dataProvider, setDataProvider] = useState(null);
+    useEffect(()=>{
+        buildGraphQLProvider({
+            client: {
+                networkInterface: createNetworkInterface({
+                    uri: 'https://api.github.com/graphql',
+                }),
+            },
+        }).then(dataProvider =>  setDataProvider(dataProvider));
+    },[])
+        return <Admin dataProvider={dataProvider} authProvider={authProvider} dashboard={Dashboard}>
+                <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+                <Resource name="users" list={UserList} icon={UserIcon} />
+        </Admin>
+};
 
 export default App;
